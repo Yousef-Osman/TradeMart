a054aeed7f350a22ece77be636910e3bba29a9d1
Yousef Osman <yousef.osman.31@gmail.com>
2023-11-27 10:17:41 +0200

    seed data

--- TradeMart.API/Program.cs
+++ TradeMart.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TradeMart.Infrastructure.Data;
 
@@ -26,5 +27,26 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<AppDbContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    //var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+    //var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+    //await context.Database.MigrateAsync();
+    //await DatabaseSeeder.SeedRolesAsync(roleManager);
+    //await DatabaseSeeder.SeedSuperAdminAsync(userManager, roleManager);
+    await DatabaseSeeder.SeedProductDataAsync(context);
+
+    logger.LogInformation("Data seeded successfully");
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "An error occured during seeding data");
+}
 
 app.Run();
---
+++ TradeMart.Infrastructure/Data/DatabaseSeeder.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using TradeMart.Domian.Entities;
+
+namespace TradeMart.Infrastructure.Data;
+public class DatabaseSeeder
+{
+    public static async Task SeedProductDataAsync(AppDbContext context)
+    {
+        if (!context.Brands.Any())
+        {
+            var brandsData = File.ReadAllText("../TradeMart.Infrastructure/Data/SeedData/brands.json");
+            var brands = JsonSerializer.Deserialize<List<Brand>>(brandsData);
+            context.Brands.AddRange(brands);
+        }
+
+        if (!context.Categories.Any())
+        {
+            var categoriesData = File.ReadAllText("../TradeMart.Infrastructure/Data/SeedData/categories.json");
+            var categories = JsonSerializer.Deserialize<List<Category>>(categoriesData);
+            context.Categories.AddRange(categories);
+        }
+
+        if (!context.Products.Any())
+        {
+            var productsData = File.ReadAllText("../TradeMart.Infrastructure/Data/SeedData/products.json");
+            var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+            context.Products.AddRange(products);
+        }
+
+        if (!context.ProductCategories.Any())
+        {
+            var productCategoriesData = File.ReadAllText("../TradeMart.Infrastructure/Data/SeedData/productCategories.json");
+            var productCategories = JsonSerializer.Deserialize<List<ProductCategory>>(productCategoriesData);
+            context.ProductCategories.AddRange(productCategories);
+        }
+
+        if (context.ChangeTracker.HasChanges())
+            await context.SaveChangesAsync();
+    }
+}
