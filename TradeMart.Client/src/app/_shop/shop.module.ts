03b6967fe98a77c023c7f815361331d92a40cbc2
Yousef Osman <yousef.osman.31@gmail.com>
2023-12-05 09:38:19 +0200

    update UI

--- TradeMart.Client/src/app/_shop/shop.module.ts
+++ TradeMart.Client/src/app/_shop/shop.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ShopRoutingModule } from './shop-routing.module';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { SidebarFiltersComponent } from './sidebar-filters/sidebar-filters.component';
 
 
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductItemComponent,
+    SidebarFiltersComponent
   ],
   imports: [
     CommonModule,
---
+++ TradeMart.Client/src/app/_shop/sidebar-filters/sidebar-filters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-filters',
+  templateUrl: './sidebar-filters.component.html',
+  styleUrl: './sidebar-filters.component.scss'
+})
+export class SidebarFiltersComponent {
+
+}
