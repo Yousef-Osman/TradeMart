6b082a0477551e333ac21eca8f5e6926742970ff
Yousef Osman <yousef.osman.31@gmail.com>
2023-12-18 11:32:04 +0200

    add shopping cart entities

---
+++ TradeMart.Domian/RedisEntities/CartItem.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TradeMart.Domian.RedisEntities;
+public class CartItem
+{
+    public CartItem()
+    {
+        Id = Guid.NewGuid().ToString();
+        Created = DateTime.Now;
+        LastModified = DateTime.Now;
+    }
+
+    [StringLength(50)]
+    public string Id { get; set; }
+
+    [StringLength(100)]
+    public string ProductName { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    [StringLength(1000)]
+    public string imageUrl { get; set; }
+
+    [StringLength(100)]
+    public string Brand { get; set; }
+
+    [StringLength(100)]
+    public string Category { get; set; }
+
+    public DateTime Created { get; set; }
+
+    public DateTime? LastModified { get; set; }
+}
---
+++ TradeMart.Domian/RedisEntities/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeMart.Domian.RedisEntities;
+public class ShoppingCart
+{
+    public ShoppingCart()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
+
+    public ShoppingCart(string id)
+    {
+        Id = id;
+    }
+
+    [StringLength(50)]
+    public string Id { get; set; }
+    public List<CartItem> Items { get; set; } = new List<CartItem>();
+}
